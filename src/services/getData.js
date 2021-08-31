6fd8f62fbe94c70c1e075a0305eae6937970059f
Vitya <vitya199600@gmail.com>
2021-08-31 11:07:23 +0300

    add build

--- src/services/getData.js
+++
@@ -1,8 +0,0 @@
-import { PARAM_PAGE } from "../utils/consts";
-
-export const getPageId = url => {
-   const pos = url.lastIndexOf(PARAM_PAGE)
-   const id = url.slice(pos + PARAM_PAGE.length,);
-
-   return Number(id);
-}
